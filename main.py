a075b65b1b58d3a89df5e8c51032230f7522ca77
pedro.a.ortega <pedro.a.ortega@accenture.com>
2022-09-28 18:42:00 -0500

    cambia agregan variables al conf

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import configparser
 from snowflake.snowpark import Session
+from snowflake.snowpark.functions import udf
 import os
 
-from snowflake.snowpark.functions import udf
 
-# connection_parameters = {
-#     "account": os.environ["snowflake_account"],
-#     "user": os.environ["snowflake_user"],
-#     "password": os.environ["snowflake_password"],
-#     "role": os.environ["snowflake_user_role"],
-#     "warehouse": os.environ["snowflake_warehouse"],
-#     "database": os.environ["snowflake_database"],
-#     "schema": os.environ["snowflake_schema"]
-# }
 
 print("===> reading config file")
 config = configparser.ConfigParser()
